4c6cafbe4268ff9c9dfe870bba62240d88a73475
Anders Ingemann <aim@orbit.online>
2022-11-28 13:54:00 +0100

    bitwarden-fields: Finally fix issue with read shadowing the jq exit code
    Now we exit with $?=1 and an error message, when a field is null / cannot be found.
    We also only cache the entry if everything went well.

--- bitwarden-fields.sh
+++ bitwarden-fields.sh
@@ -140,7 +140,9 @@ for ((;docopt_i>0;docopt_i--)); do declare -p "${prefix}__cache_for" \
   exec 9<>"$lockpath"
   flock 9
   trap "exec 9>&-" EXIT
+  local was_cached=true
   if ! data=$(socket-credential-cache get "$cache_name" 2>/dev/null); then
+    was_cached=false
     if [[ -z $BW_SESSION ]]; then
       export BW_SESSION
       BW_SESSION=$(bitwarden-unlock --purpose="retrieve \"$ITEMNAME\"")
@@ -149,7 +151,7 @@ for ((;docopt_i>0;docopt_i--)); do declare -p "${prefix}__cache_for" \
     fi
     if ! data=$(bw --nointeraction --raw get item "$ITEMNAME"); then
       local ret=$?
-      printf "Unable to retrieve '%s'\n" "$ITEMNAME" >&2
+      printf "bitwarden-fields.sh: Unable to retrieve '%s'\n" "$ITEMNAME" >&2
       return $ret
     fi
     local item_id
@@ -168,39 +170,48 @@ for ((;docopt_i>0;docopt_i--)); do declare -p "${prefix}__cache_for" \
       )
     done
     unset BW_SESSION
-    # shellcheck disable=2154
-    if [[ $__cache_for -gt 0 ]]; then
-      socket-credential-cache --timeout="$__cache_for" set "$cache_name" <<<"$data"
-    fi
   fi
   # shellcheck disable=2154
   if $__json; then
     local json_out='{}'
   fi
   if [[ ${#FIELD[@]} -eq 0 ]]; then
+    # No fields specified, output everything
     readarray -td $'\n' FIELD < <(
-      jq -r '((.login // {}) | keys[]), ((.attachments // [])[] | ("attachment:" + .fileName)), ((.fields // [])[] | .name)' <<<"$data"
+      jq -r '((.login // {}) | del(.[] | nulls) | keys[]), ((.attachments // [])[] | ("attachment:" + .fileName)), ((.fields // [])[] | .name)' <<<"$data"
     )
   fi
   local field_name
   for field_name in "${FIELD[@]}"; do
     local variable_name=$field_name
-    # We read $value weirdly to preserve trailing newlines. Command substitution removes all trailing newlines
+    # Command substitution removes all trailing newlines, so we append an ETX (end of text) char and then remove it afterwards
     if [[ $field_name = username || $field_name = password || $field_name = totp || $field_name = uris || $field_name = passwordRevisionDate ]]; then
-      IFS= read -rd '' value < <(jq -r --arg name "$field_name" '.login[$name]' <<<"$data") || true
+      if ! value=$(jq -jre --arg name "$field_name" '.login[$name]' <<<"$data" && printf '\3'); then
+        printf "bitwarden-fields.sh: The field %s is not set.\n" "$field_name" >&2
+        return 1
+      fi
     elif [[ $field_name = attachmentid:* ]]; then
       local attachment_id=${field_name/#attachmentid:/}
       variable_name=${variable_name/#attachmentid:/}
-      IFS= read -rd '' value < <(jq -r --arg id "$attachment_id" '.attachments[] | select(.id==$id).data' <<<"$data") || true
+      if ! value=$(jq -jre --arg id "$attachment_id" '.attachments[] | select(.id==$id).data' <<<"$data" && printf '\3'); then
+        printf "bitwarden-fields.sh: The attachment %s does not exist.\n" "$attachment_id" >&2
+        return 1
+      fi
     elif [[ $field_name = attachment:* ]]; then
       local attachment_name=${field_name/#attachment:/}
       variable_name=${variable_name/#attachment:/}
-      IFS= read -rd '' value < <( jq -r --arg name "$attachment_name" '.attachments[] | select(.fileName==$name).data' <<<"$data" ) || true
+      if ! value=$(jq -jre --arg name "$attachment_name" '.attachments[] | select(.fileName==$name).data' <<<"$data" && printf '\3'); then
+        printf "bitwarden-fields.sh: The attachment %s does not exist.\n" "$attachment_name" >&2
+        return 1
+      fi
     else
-      IFS= read -rd '' value < <(jq -r --arg name "$field_name" '.fields[] | select(.name==$name).value' <<<"$data") || true
+      if ! value=$(jq -jre --arg name "$field_name" '.fields[] | select(.name==$name).value' <<<"$data" && printf '\3'); then
+        printf "bitwarden-fields.sh: The field %s is not set.\n" "$field_name" >&2
+        return 1
+      fi
     fi
-    # jq itself attaches a newline to its output, remove that, but keep others
-    value=${value%$'\n'}
+    # Remove the ETX char
+    value=${value%$'\3'}
     if $__json; then
       json_out=$(jq --arg key "$variable_name" --arg value "$value" '.[$key]=$value' <<<"$json_out")
     else
@@ -211,6 +222,10 @@ for ((;docopt_i>0;docopt_i--)); do declare -p "${prefix}__cache_for" \
   done
   if $__json; then
     printf "%s\n" "$json_out"
+  fi
+  # shellcheck disable=2154
+  if ! $was_cached && [[ $__cache_for -gt 0 ]]; then
+    socket-credential-cache --timeout="$__cache_for" set "$cache_name" <<<"$data"
   fi
 }
 
