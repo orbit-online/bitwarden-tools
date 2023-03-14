b0499700c0af43157253a4ce46ee1fc65243a369
Anders Ingemann <aim@orbit.online>
2023-03-14 10:09:56 +0100

    bitwarden-fields: Add --prefix and -e options
    Also prevent stdin swallowing when bw is backed by docker

--- bitwarden-fields.sh
+++ bitwarden-fields.sh
@@ -7,8 +7,10 @@ Usage:
   bitwarden-fields [options] ITEMNAME [FIELD...]
 
 Options:
-  --cache-for=SECONDS  Cache item for retrieval without a session [default: 0]
-  --json -j            Output as JSON instead of bash variables
+  --cache-for=SECONDS  Cache item with socket-credential-cache [default: 0]
+  -j --json            Output as JSON instead of bash variables
+  --prefix=PREFIX      Prefix variable names with supplied string
+  -e                   Print 'false' on error
   --debug              Turn on bash -x
 Note:
   To retrieve attachments, prefix their name with \`attachment:\`
@@ -101,28 +103,32 @@ eval "var_$1+=($value)"; else eval "var_$1=$value"; fi; return 0; fi; done
 return 1; }; stdout() { printf -- "cat <<'EOM'\n%s\nEOM\n" "$1"; }; stderr() {
 printf -- "cat <<'EOM' >&2\n%s\nEOM\n" "$1"; }; error() {
 [[ -n $1 ]] && stderr "$1"; stderr "$usage"; _return 1; }; _return() {
-printf -- "exit %d\n" "$1"; exit "$1"; }; set -e; trimmed_doc=${DOC:0:464}
-usage=${DOC:47:55}; digest=c886f; shorts=('' '' -j)
-longs=(--cache-for --debug --json); argcounts=(1 0 0); node_0(){
-value __cache_for 0; }; node_1(){ switch __debug 1; }; node_2(){ switch __json 2
-}; node_3(){ value ITEMNAME a; }; node_4(){ value FIELD a true; }; node_5(){
-optional 0 1 2; }; node_6(){ optional 5; }; node_7(){ oneormore 4; }; node_8(){
-optional 7; }; node_9(){ required 6 3 8; }; node_10(){ required 9; }
-cat <<<' docopt_exit() { [[ -n $1 ]] && printf "%s\n" "$1" >&2
+printf -- "exit %d\n" "$1"; exit "$1"; }; set -e; trimmed_doc=${DOC:0:573}
+usage=${DOC:47:55}; digest=86bcb; shorts=('' '' -j '' -e)
+longs=(--cache-for --prefix --json --debug ''); argcounts=(1 1 0 0 0); node_0(){
+value __cache_for 0; }; node_1(){ value __prefix 1; }; node_2(){ switch __json 2
+}; node_3(){ switch __debug 3; }; node_4(){ switch _e 4; }; node_5(){
+value ITEMNAME a; }; node_6(){ value FIELD a true; }; node_7(){
+optional 0 1 2 3 4; }; node_8(){ optional 7; }; node_9(){ oneormore 6; }
+node_10(){ optional 9; }; node_11(){ required 8 5 10; }; node_12(){ required 11
+}; cat <<<' docopt_exit() { [[ -n $1 ]] && printf "%s\n" "$1" >&2
 printf "%s\n" "${DOC:47:55}" >&2; exit 1; }'; unset var___cache_for \
-var___debug var___json var_ITEMNAME var_FIELD; parse 10 "$@"
+var___prefix var___json var___debug var__e var_ITEMNAME var_FIELD; parse 12 "$@"
 local prefix=${DOCOPT_PREFIX:-''}; unset "${prefix}__cache_for" \
-"${prefix}__debug" "${prefix}__json" "${prefix}ITEMNAME" "${prefix}FIELD"
+"${prefix}__prefix" "${prefix}__json" "${prefix}__debug" "${prefix}_e" \
+"${prefix}ITEMNAME" "${prefix}FIELD"
 eval "${prefix}"'__cache_for=${var___cache_for:-0}'
-eval "${prefix}"'__debug=${var___debug:-false}'
+eval "${prefix}"'__prefix=${var___prefix:-}'
 eval "${prefix}"'__json=${var___json:-false}'
+eval "${prefix}"'__debug=${var___debug:-false}'
+eval "${prefix}"'_e=${var__e:-false}'
 eval "${prefix}"'ITEMNAME=${var_ITEMNAME:-}'
 if declare -p var_FIELD >/dev/null 2>&1; then
 eval "${prefix}"'FIELD=("${var_FIELD[@]}")'; else eval "${prefix}"'FIELD=()'; fi
 local docopt_i=1; [[ $BASH_VERSION =~ ^4.3 ]] && docopt_i=2
 for ((;docopt_i>0;docopt_i--)); do declare -p "${prefix}__cache_for" \
-"${prefix}__debug" "${prefix}__json" "${prefix}ITEMNAME" "${prefix}FIELD"; done
-}
+"${prefix}__prefix" "${prefix}__json" "${prefix}__debug" "${prefix}_e" \
+"${prefix}ITEMNAME" "${prefix}FIELD"; done; }
 # docopt parser above, complete command for generating this parser is `docopt.sh bitwarden-fields.sh`
 
   checkdeps bw jq
@@ -147,12 +153,13 @@ for ((;docopt_i>0;docopt_i--)); do declare -p "${prefix}__cache_for" \
       export BW_SESSION
       BW_SESSION=$(bitwarden-unlock --purpose="retrieve \"$ITEMNAME\"")
       # shellcheck disable=2064
-      trap "exec 9>&-; BW_SESSION=\"$BW_SESSION\" bw lock >/dev/null" EXIT
+      trap "exec 9>&-; BW_SESSION=\"$BW_SESSION\" bw lock </dev/null >/dev/null" EXIT
     fi
-    if ! data=$(bw --nointeraction --raw get item "$ITEMNAME"); then
-      local ret=$?
-      printf "bitwarden-fields.sh: Unable to retrieve '%s'\n" "$ITEMNAME" >&2
-      return $ret
+    if ! data=$(bw --nointeraction --raw get item "$ITEMNAME" </dev/null); then
+      printf "\nbitwarden-fields.sh: Unable to retrieve '%s'\n" "$ITEMNAME" >&2
+      # shellcheck disable=2154
+      [[ $_e != 'true' ]] || printf "false\n"
+      return 1
     fi
     local item_id
     item_id=$(jq -r '.id' <<<"$data")
@@ -160,7 +167,7 @@ for ((;docopt_i>0;docopt_i--)); do declare -p "${prefix}__cache_for" \
     local attachment_path
     for attachment_id in $(jq -r '(.attachments // [])[].id' <<<"$data"); do
       attachment_path=$(mktemp)
-      bw --nointeraction --quiet get attachment "$attachment_id" --itemid "$item_id" --output "$attachment_path"
+      bw --nointeraction --quiet get attachment "$attachment_id" --itemid "$item_id" --output "$attachment_path" </dev/null
       data=$(
         jq --arg id "$attachment_id" '.attachments[(.attachments | map(.id == $id) | index(true))].data = '"$(jq --slurp -R . "$attachment_path")" \
         <<<"$data"
@@ -185,9 +192,16 @@ for ((;docopt_i>0;docopt_i--)); do declare -p "${prefix}__cache_for" \
   for field_name in "${FIELD[@]}"; do
     local variable_name=$field_name
     # Command substitution removes all trailing newlines, so we append an ETX (end of text) char and then remove it afterwards
-    if [[ $field_name = username || $field_name = password || $field_name = totp || $field_name = uris || $field_name = passwordRevisionDate ]]; then
+    if [[ $field_name = id ]]; then
+      if ! value=$(jq -jre '.id' <<<"$data" && printf '\3'); then
+        printf "bitwarden-fields.sh: Unable to retried the ID field.\n" >&2
+        [[ $_e != 'true' ]] || printf "false\n"
+        return 1
+      fi
+    elif [[ $field_name = username || $field_name = password || $field_name = totp || $field_name = uris || $field_name = passwordRevisionDate ]]; then
       if ! value=$(jq -jre --arg name "$field_name" '.login[$name]' <<<"$data" && printf '\3'); then
         printf "bitwarden-fields.sh: The field %s is not set.\n" "$field_name" >&2
+        [[ $_e != 'true' ]] || printf "false\n"
         return 1
       fi
     elif [[ $field_name = attachmentid:* ]]; then
@@ -195,6 +209,7 @@ for ((;docopt_i>0;docopt_i--)); do declare -p "${prefix}__cache_for" \
       variable_name=${variable_name/#attachmentid:/}
       if ! value=$(jq -jre --arg id "$attachment_id" '.attachments[] | select(.id==$id).data' <<<"$data" && printf '\3'); then
         printf "bitwarden-fields.sh: The attachment %s does not exist.\n" "$attachment_id" >&2
+        [[ $_e != 'true' ]] || printf "false\n"
         return 1
       fi
     elif [[ $field_name = attachment:* ]]; then
@@ -202,16 +217,20 @@ for ((;docopt_i>0;docopt_i--)); do declare -p "${prefix}__cache_for" \
       variable_name=${variable_name/#attachment:/}
       if ! value=$(jq -jre --arg name "$attachment_name" '.attachments[] | select(.fileName==$name).data' <<<"$data" && printf '\3'); then
         printf "bitwarden-fields.sh: The attachment %s does not exist.\n" "$attachment_name" >&2
+        [[ $_e != 'true' ]] || printf "false\n"
         return 1
       fi
     else
       if ! value=$(jq -jre --arg name "$field_name" '.fields[] | select(.name==$name).value' <<<"$data" && printf '\3'); then
         printf "bitwarden-fields.sh: The field %s is not set.\n" "$field_name" >&2
+        [[ $_e != 'true' ]] || printf "false\n"
         return 1
       fi
     fi
     # Remove the ETX char
     value=${value%$'\3'}
+    # shellcheck disable=2154
+    variable_name=$__prefix$variable_name
     if $__json; then
       json_out=$(jq --arg key "$variable_name" --arg value "$value" '.[$key]=$value' <<<"$json_out")
     else
